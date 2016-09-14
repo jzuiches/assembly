028a7d54fe9aa2ac0f50841fce7f8980e846d08a
Joe Zuiches <jzuiches@yahoo.com>
2016-09-14 14:17:25 -0700

    images to landing screen

--- application/components/Landing.js
+++ application/components/Landing.js
@@ -1,14 +1,21 @@
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import React, { Component } from 'react';
 import {
   View,
   Text,
+  Image,
   TouchableOpacity
  } from 'react-native';
 
-import Icon from 'react-native-vector-icons/Ionicons';
-import NavigationBar from 'react-native-navbar';
+
+import { landingStyles } from '../styles';
 import Colors from '../styles/colors';
 import { globals } from '../styles';
+
+
+const BackgroundImage = 'https://s3-us-west-2.amazonaws.com/assembliesapp/welcome%402x.png';
+const Logo = 'https://s3-us-west-2.amazonaws.com/assembliesapp/logo.png';
+const styles = landingStyles;
 
 class Landing extends Component{
 
@@ -23,26 +30,40 @@ class Landing extends Component{
   }
 
   render(){
-    let titleConfig = { title: 'Landing', tintColor: 'black'};
+
     return(
+      <View style={styles.container}>
+      <View style={styles.container}>
+        <Image
+          style={styles.backgroundImage}
+          source={require('../assets/images/welcome.png')}
+        />
+      </View>
+        <View style={globals.flexCenter}>
+        <Image
+          style={styles.logo}
+          source={{uri:Logo}}
+        />
+        <Text style={[globals.lightText, globals.h2, globals.mb2]}>
+          assemblies
+        </Text>
+        <Text style={[globals.lightText, globals.h4]}>
+          Where Developers Connect
+        </Text>
+      </View>
+      <TouchableOpacity
+         style={globals.button}
+         onPress={this.visitDashboard}
+       >
+         <Icon name='person' size={36} color='white' />
+         <Text style={globals.buttonText}>
+           Go to Dashboard
+         </Text>
+       </TouchableOpacity>
 
-          <View style={globals.flexContainer}>
-          <NavigationBar
-             title={titleConfig}
-             tintColor={Colors.brandPrimary}
-           />
-            <View style={globals.flexCenter}>
-            <Text style={globals.h2}>
-              This is the Landing Page
-            </Text>
-            <TouchableOpacity onPress={this.visitDashboard}>
-              <Text>Go to the Dashboard</Text>
-            </TouchableOpacity>
-            </View>
-          </View>
-  )
-        }
-
+      </View>
+    )
+  }
 }
 
 export default Landing;
